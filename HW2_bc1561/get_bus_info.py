bd29e72d928444188485590e82825db65a09858c
SPTKL <caobaiyue@gmail.com>
2017-09-21 18:29:26 -0400

    add hw2_2

--- HW2_bc1561/get_bus_info.py
+++ HW2_bc1561/get_bus_info.py
@@ -11,26 +11,30 @@ except ImportError:
 info_input = sys.argv
 my_key = info_input[1]
 bus_line = info_input[2]
+file_name = info_input[3]
 PUIdata = os.getenv("PUIDATA")
 url = 'http://bustime.mta.info/api/siri/vehicle-monitoring.json?key=' + my_key + '&VehicleMonitoringDetailLevel=calls&LineRef=' + bus_line
 response = urllib.urlopen(url)
 data = response.read().decode("utf-8")
 data = json.loads(data)
+fout = open(file_name, "w")
+fout.write('Latitude,Longitude,Stop Name,Stop Status\n')
 
 def print_info(bus_line, data):
     """
-    given a bus line, print the bus name,
-    number of vehicles and the current position
+    given a bus line, save operating buses info into csv,
+    with Latitude,Longitude,Stop Name,Stop Status as column names
     """
     bus_info = data['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity']
     bus_count = len(bus_info)
-    print(bus_line)
-    print('Number of Active Buses : ' + str(len(bus_info)))
     for i in range(bus_count):
         location = bus_info[i]['MonitoredVehicleJourney']['VehicleLocation']
         lat = location['Latitude']
         lon = location['Longitude']
         position_info = 'Bus ' + str(i) + ' is at latitude ' + str(lat) + ' and longitude ' + str(lon)
-        print(position_info)
-
+        stop_info = bus_info[i]['MonitoredVehicleJourney']['OnwardCalls']['OnwardCall'][0]
+        stop_name = stop_info['StopPointName']
+        stop_status = stop_info['Extensions']['Distances']['PresentableDistance']
+        line = str(lat) + ',' + str(lon) + ',' + stop_name + ',' + stop_status +'\n'
+        fout.write(line)
 print_info(bus_line, data)
